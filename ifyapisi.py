0c30e65de9c6997c487b21d74e95e4a5cceef06c
zorbeyyavas <yavaszorbey@gmail.com>
2024-10-12 16:03:51 +0300

    Add files via upload

---
+++ ifyapisi.py
@@ -0,0 +1,6 @@
+#pythonda if,şartın sonuna 2nokta üst üste : koy ayrıca else ifadesinin sonunada koy
+isim="mehmet"
+if(isim=="ahmet"):
+    print("merhaba",isim)
+else:
+    print("sen ahmet değilsin")
